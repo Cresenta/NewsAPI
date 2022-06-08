38c59563b6ea65773c5efc8460919c3fdd6208a8
Davin Leo <davinleo.96@gmail.com>
2022-06-08 15:17:20 +0700

    Added GetSourcesUseCase.kt

--- api_service/src/main/java/id/indocyber/api_service/usecase/GetSourcesUseCase.kt
+++ api_service/src/main/java/id/indocyber/api_service/usecase/GetSourcesUseCase.kt
@@ -1,4 +1,22 @@
 package id.indocyber.api_service.usecase
 
-class GetSourcesUseCase {
+import id.indocyber.api_service.service.GetSourcesService
+import id.indocyber.common.AppResponse
+import kotlinx.coroutines.flow.flow
+
+class GetSourcesUseCase(
+    private val getSourcesService: GetSourcesService
+) {
+    operator fun invoke(category: String) = flow {
+        val response = getSourcesService.getSources(
+            category = category
+        )
+        if (response.isSuccessful) {
+            response.body()?.let { data ->
+                this.emit(AppResponse.AppResponseSuccess(data))
+            } ?: this.emit(AppResponse.AppResponseError(null))
+        } else {
+            this.emit(AppResponse.AppResponseError(null))
+        }
+    }
 }
